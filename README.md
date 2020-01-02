535c2103324f5d90f11211996d26a07054fdf3a2
lihex <lihe757@aliyun.com>
2020-01-02 14:03:02 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+cd learning_log
+python -m venv ll__env
+virtualenv ll_env
+source ll_env/bin/activate
+pip install Django
+python manage.py runserver
---
+++ learning_logs/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Topic
+
+def index(request):
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('data_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
