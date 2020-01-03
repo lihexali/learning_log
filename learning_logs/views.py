55d7a35df1e011125482178d72e905361c48e75b
lihexali <hexuan.li@cocos2d-x.org>
2020-01-03 20:27:30 +0800

    add new topic function

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Topic
+from .forms import TopicForm
+
 
 def index(request):
     return render(request, 'learning_logs/index.html')
@@ -16,3 +20,14 @@ def topic(request, topic_id):
     context = {'topic' : topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
