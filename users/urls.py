34cc9ae4d8567846f3aabd5545772bfa3f9a6507
lihexali <hexuan.li@cocos2d-x.org>
2020-01-06 11:10:34 +0800

    add register function

--- users/urls.py
+++ users/urls.py
@@ -23,4 +23,5 @@ from . import views
 urlpatterns = [
         url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
         url(r'^logout/$', views.logout_view, name='logout'),
+        url(r'^register/$', views.register, name='register'),
 ]
